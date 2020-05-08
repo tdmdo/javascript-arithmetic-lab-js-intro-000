8634415628f492525bbfa9b9fb5d5480b64cc683
Daniel Do <daniel.minh.do@gmail.com>
2020-05-08 03:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ var number = 10
 
 function add(number)
   {number += (number)}
+
+function subtract(number)
+  {number -= (number)}
+
+function multiple(number)
+  {number *= (number)}
+
+function divide(number)
+  {number /= (number)}
